004c9ec427298710d48cac00c1665004262c722e
jatin.sachdeva <sachdeva.jatin.dev@gmail.com>
2019-07-20 23:06:05 +0530

    Rx java + Rx java adapter + Session Manager + MainActivity

---
+++ app/src/main/java/com/stunnningcoder/daggerinitials/login/User.kt
@@ -0,0 +1,7 @@
+package com.stunnningcoder.daggerinitials.login
+
+/**
+ * Created by Jatin on 7/14/19.
+ */
+class User {
+}
---
+++ app/src/main/java/com/stunnningcoder/daggerinitials/network/NetworkResource.kt
@@ -0,0 +1,7 @@
+package com.stunnningcoder.daggerinitials.network
+
+/**
+ * Created by Jatin on 7/17/19.
+ */
+class NetworkResource {
+}
